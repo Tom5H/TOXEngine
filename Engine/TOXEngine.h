190421a314befe9366eaa5aee192421a7c99a9ac
Tom5H <tomhofer@protonmail.com>
2023-05-17 22:33:07 +0200

    removed unused function

--- Engine/TOXEngine.h
+++ Engine/TOXEngine.h
@@ -178,12 +178,6 @@ private:
     // glfwTerminate();
   }
 
-  // unused function
-  // bool hasStencilComponent(VkFormat format) {
-  //  return format == VK_FORMAT_D32_SFLOAT_S8_UINT ||
-  //         format == VK_FORMAT_D24_UNORM_S8_UINT;
-  //}
-
   void createTextureSampler() {
     VkPhysicalDeviceProperties properties{};
     vkGetPhysicalDeviceProperties(physicalDevice->get(), &properties);
